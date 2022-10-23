8e8a4d0425ac833c4a560ca91184faadf0dbd8a8
AgusZ02 <76200509+AgusZ02@users.noreply.github.com>
2022-10-23 18:08:35 +0200

    Cargar los ficheros

--- AGarciaOGonzaloJARodriguez/src/java/CatalogoIMDB.java
+++ AGarciaOGonzaloJARodriguez/src/java/CatalogoIMDB.java
@@ -1,11 +1,14 @@
 package java;
 
+import java.io.DataInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class CatalogoIMDB {
 
 private static CatalogoIMDB miCatalogo;
-    
-    
-    
+
     private ListaPeliculas listaPeliculas;
     private ListaInterpretes listaInterpretes;
     
@@ -35,23 +38,47 @@ private static CatalogoIMDB miCatalogo;
     /**
     * Carga las pel�culas del cat�logo desde el fichero indicado
     * @param nomF Nombre del fichero que contiene las pel�culas
+     * @throws FileNotFoundException
     */
-    public void cargarPeliculas(String nomF) {// Ver ayuda en siguiente apartado
-    /**
-    * Carga los int�rpretes del cat�logo desde el fichero indicado
-    * POST: se han cargado los int�rpretes y se han calculado sus ratings
-    * @param nomF Nombre del fichero que contiene los int�rpretes
-    */
-    }
-    
-    
-    public void cargarInterpretes(String nomF) {
-    /**
-    * Imprime por pantalla el n� de int�rpretes de una pel�cula y sus nombres
-    * @param titulo T�tulo de la pel�cula
-    */
+    public void cargarPeliculas(String nomF) throws FileNotFoundException {// Ver ayuda en siguiente apartado
+        Scanner entrada = new Scanner(new FileReader(nomF));
+        String linea = "";
+        String[] datos = new String[4]; //Array donde cada posición contendrá los datos de cada película.
+        while (entrada.hasNext()) {
+            linea = entrada.nextLine();
+            datos = linea.split("\t");
+            this.listaPeliculas.anadirPelicula(new Pelicula(datos[0], Integer.valueOf(datos[1]), Double.valueOf(datos[2]), Integer.valueOf(datos[3])));
+        }
         
     }
+    
+    /**
+     * Esta función carga todos los intérpretes de los archivos en listaInterpretes.
+     * Se lee el archivo línea por línea y se va tratando separando el artista con sus películas.
+     * @param nomF el nombre del fichero a leer.
+     * @throws FileNotFoundException Si el archivo no existe.
+     */
+    public void cargarInterpretes(String nomF) throws FileNotFoundException {
+        
+        Scanner entrada = new Scanner(new FileReader(nomF));
+        String linea = "";
+        String[] datos = linea.split("->");//Separa el nombre del intérprete de sus películas
+        ListaPeliculas pelisDeInterprete = new ListaPeliculas();
+        Pelicula peliculaActual;
+    
+        while (entrada.hasNext()) {
+            linea = entrada.nextLine();
+            String[] datosPelis = datos[1].split("\\|\\|"); //Separa todas las películas del artista y las añade a una array
+            
+            for (int i = 0; i < datosPelis.length; i++) { //Añade cada elemento del array a la estructura pelisDeInterprete.
+                peliculaActual = this.listaPeliculas.buscarPelicula(datosPelis[i]);
+                pelisDeInterprete.anadirPelicula(peliculaActual);
+            }
+            
+            this.listaInterpretes.anadirInterprete(new Interprete(datos[0], pelisDeInterprete));
+        }
+        
+    } 
     public void imprimirInfoPelicula(String titulo) {
     /**
     * Imprime por pantalla el nombre del int�rprete, su rating y los t�tulos
