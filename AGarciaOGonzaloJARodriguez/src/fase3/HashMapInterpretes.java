c162e7fa45d831b3fa61cc9f8baf09ca44e8e594
Agustin <76200509+AgusZ02@users.noreply.github.com>
2023-01-06 17:31:26 +0100

    Métodos completados

--- AGarciaOGonzaloJARodriguez/src/fase3/HashMapInterpretes.java
+++ AGarciaOGonzaloJARodriguez/src/fase3/HashMapInterpretes.java
@@ -1,9 +1,28 @@
 package fase3;
 
-public class HashMapInterpretes<K,V> implements InterfazInterpretes{
-    public void anadirInterprete(Interprete inter){}
-	public Interprete buscarInterprete (String sNombre){return null;}
-	public Interprete eliminarInterprete(String nombre){return null;}
-    public int size(){return 0;}
-	public boolean isEmpty(){return false;}
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapInterpretes implements InterfazInterpretes{
+    private Map<String,Interprete> tabla;
+    public HashMapInterpretes(){
+        this.tabla = new HashMap<String,Interprete>();
+    }
+    public void anadirInterprete(Interprete inter){
+        this.tabla.put(inter.getNombre(), inter);
+    }
+	public Interprete buscarInterprete (String sNombre){
+        return this.tabla.get(sNombre);
+    }
+	public Interprete eliminarInterprete(String nombre){
+        Interprete interprete = this.tabla.get(nombre);
+        this.tabla.put(interprete.getNombre(), null);
+        return interprete;
+    }
+    public int size(){
+        return this.tabla.size();
+    }
+	public boolean isEmpty(){
+        return this.tabla.isEmpty();
+    }
 }
